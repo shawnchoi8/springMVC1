0f55bd29d7212073429e4f3b65c8da879e40c07d
shchoilv1 <bychoise8@gmail.com>
2024-09-06 12:42:49 +0900

    logging

---
+++ springmvc/src/main/java/hello/springmvc/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    //private final Logger log = LoggerFactory.getLogger(getClass()); //Slf4j
+
+    @RequestMapping("/log-test")
+    public String LogTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
